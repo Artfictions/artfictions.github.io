6f8e0c08cdaee696c6c3c0088ad4f9d2089ec95f
James O'Sullivan <james.osullivan@ucc.ie>
2024-02-12 21:40:27 +0000

    Update search.js

--- search.js
+++ search.js
@@ -2,10 +2,6 @@ function searchBooks() {
     const searchTerm = document.getElementById('searchBox').value.toLowerCase();
     console.log("Search Term:", searchTerm); // Confirming the search term is captured correctly
 
-    const filteredBooks = books.filter(book => 
-        book.Title?.toLowerCase().includes(searchTerm) ||
-        book.Author?.toLowerCase().includes(searchTerm) ||
-        book.Themes?.some(theme => theme?.toLowerCase().includes(searchTerm))
-    );
-    
+    const filteredBooks = books.filter(book => book.Title?.toLowerCase().includes(searchTerm));
+
 }
