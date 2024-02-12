12992bfee5ee53e0099b7572417061108a315adb
James O'Sullivan <james.osullivan@ucc.ie>
2024-02-12 21:36:29 +0000

    Update search.js

--- search.js
+++ search.js
@@ -2,9 +2,10 @@ function searchBooks() {
     const searchTerm = document.getElementById('searchBox').value.toLowerCase();
     console.log("Search Term:", searchTerm); // Confirming the search term is captured correctly
 
-    const filteredBooks = books.filter(book => book.Title?.toLowerCase().includes(searchTerm));
-    console.log("Filtered Books:", filteredBooks); // Inspect the filtered results
-
-    // For now, you might comment out the displayResults function or simplify it
-    // displayResults(filteredBooks);
+    const filteredBooks = books.filter(book => 
+        book.Title?.toLowerCase().includes(searchTerm) ||
+        book.Author?.toLowerCase().includes(searchTerm) ||
+        book.Themes?.some(theme => theme?.toLowerCase().includes(searchTerm))
+    );
+    
 }
